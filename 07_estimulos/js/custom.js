48145c503f43450b77dcd681eb27a9b7e3ac575b
Paulo-SenacRS <prjunior@senacrs.com.br>
2024-04-25 20:05:28 -0300

    adicionado o botão e o áudio de água no conhecimento 7

---
+++ 07_estimulos/js/custom.js
@@ -0,0 +1,25 @@
+var FXManager = {
+    snd_certo : new Audio("objetos/snd/acerto.mp3"),
+    snd_erro : new Audio("objetos/snd/erro.mp3"),
+    snd_aplauso : new Audio("objetos/snd/aplauso.mp3")
+}
+
+
+jQuery("document").ready(function($){
+
+    var audioAgua = document.getElementById("audio-agua");
+    var btnAgua = '0';
+
+    $(".btn-audio_agua").click(function(){
+        audioAgua.volume = 0.15;
+    });
+    $(".btn-play_audio").click(function(){
+        if(btnAgua == '0'){
+            audioAgua.play();
+            btnAgua = '1';
+        }else if(btnAgua == '1'){
+            audioAgua.pause();
+            btnAgua = '0';
+        }
+    });
+});
